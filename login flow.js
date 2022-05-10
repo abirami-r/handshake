fa793cba1a9d63890c68c058146ce41379a1fdfb
Abirami Ravishankar <41388986+abirami-r@users.noreply.github.com>
2022-05-10 12:25:25 -0400

    Add files via upload

---
+++ login flow.js
@@ -0,0 +1,150 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    async function waitForSelectors(selectors, frame) {
+      for (const selector of selectors) {
+        try {
+          return await waitForSelector(selector, frame);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      throw new Error('Could not find element for selectors: ' + JSON.stringify(selectors));
+    }
+
+    async function waitForSelector(selector, frame) {
+      if (selector instanceof Array) {
+        let element = null;
+        for (const part of selector) {
+          if (!element) {
+            element = await frame.waitForSelector(part);
+          } else {
+            element = await element.$(part);
+          }
+          if (!element) {
+            throw new Error('Could not find element: ' + part);
+          }
+          element = (await element.evaluateHandle(el => el.shadowRoot ? el.shadowRoot : el)).asElement();
+        }
+        if (!element) {
+          throw new Error('Could not find element: ' + selector.join('|'));
+        }
+        return element;
+      }
+      const element = await frame.waitForSelector(selector);
+      if (!element) {
+        throw new Error('Could not find element: ' + selector);
+      }
+      return element;
+    }
+
+    async function waitForElement(step, frame) {
+      const count = step.count || 1;
+      const operator = step.operator || '>=';
+      const comp = {
+        '==': (a, b) => a === b,
+        '>=': (a, b) => a >= b,
+        '<=': (a, b) => a <= b,
+      };
+      const compFn = comp[operator];
+      await waitForFunction(async () => {
+        const elements = await querySelectorsAll(step.selectors, frame);
+        return compFn(elements.length, count);
+      });
+    }
+
+    async function querySelectorsAll(selectors, frame) {
+      for (const selector of selectors) {
+        const result = await querySelectorAll(selector, frame);
+        if (result.length) {
+          return result;
+        }
+      }
+      return [];
+    }
+
+    async function querySelectorAll(selector, frame) {
+      if (selector instanceof Array) {
+        let elements = [];
+        let i = 0;
+        for (const part of selector) {
+          if (i === 0) {
+            elements = await frame.$$(part);
+          } else {
+            const tmpElements = elements;
+            elements = [];
+            for (const el of tmpElements) {
+              elements.push(...(await el.$$(part)));
+            }
+          }
+          if (elements.length === 0) {
+            return [];
+          }
+          const tmpElements = [];
+          for (const el of elements) {
+            const newEl = (await el.evaluateHandle(el => el.shadowRoot ? el.shadowRoot : el)).asElement();
+            if (newEl) {
+              tmpElements.push(newEl);
+            }
+          }
+          elements = tmpElements;
+          i++;
+        }
+        return elements;
+      }
+      const element = await frame.$$(selector);
+      if (!element) {
+        throw new Error('Could not find element: ' + selector);
+      }
+      return element;
+    }
+
+    async function waitForFunction(fn) {
+      let isActive = true;
+      setTimeout(() => {
+        isActive = false;
+      }, 5000);
+      while (isActive) {
+        const result = await fn();
+        if (result) {
+          return;
+        }
+        await new Promise(resolve => setTimeout(resolve, 100));
+      }
+      throw new Error('Timed out');
+    }
+    {
+        const targetPage = page;
+        await targetPage.setViewport({"width":645,"height":622})
+    }
+    {
+        const targetPage = page;
+        const promises = [];
+        promises.push(targetPage.waitForNavigation());
+        await targetPage.goto('https://app.joinhandshake.com/login');
+        await Promise.all(promises);
+    }
+    {
+        const targetPage = page;
+        const promises = [];
+        promises.push(targetPage.waitForNavigation());
+        const element = await waitForSelectors([["#s2id_autogen1_search"]], targetPage);
+        const type = await element.evaluate(el => el.type);
+        if (["textarea","select-one","text","url","tel","search","password","number","email"].includes(type)) {
+          await element.type('new york univers');
+        } else {
+          await element.focus();
+          await element.evaluate((el, value) => {
+            el.value = value;
+            el.dispatchEvent(new Event('input', { bubbles: true }));
+            el.dispatchEvent(new Event('change', { bubbles: true }));
+          }, "new york univers");
+        }
+        await Promise.all(promises);
+    }
+
+    await browser.close();
+})();
